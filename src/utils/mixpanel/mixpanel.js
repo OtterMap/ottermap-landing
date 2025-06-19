ba7eef9df546cc58658f0d8ff06e7f7c57142aff
Shiv Chauhan <shiv.chauhan@ottermap.com>
2025-06-19 12:09:30 +0530

    mixpanel logs removed

--- src/utils/mixpanel/mixpanel.js
+++ src/utils/mixpanel/mixpanel.js
@@ -3,7 +3,7 @@ import mixpanel from "mixpanel-browser";
 const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
 mixpanel.init(MIXPANEL_TOKEN, {
     track_pageview: true,
-    debug: true,
+    // debug: true,
     persistence: 'localStorage',
     ip: true,
     autocapture: true
